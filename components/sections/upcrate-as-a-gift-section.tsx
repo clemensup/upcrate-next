e63e983c0301783428e44be446c7572b1e31ec43
David <info@davidbucka.com>
2021-08-20 09:59:06 +0200

    translate: upcrate as gift section

--- components/sections/upcrate-as-a-gift-section.tsx
+++ components/sections/upcrate-as-a-gift-section.tsx
@@ -1,16 +1,19 @@
+import useTranslation from "next-translate/useTranslation";
 import { Button } from "../elements/button";
 
 export function UpcrateAsGiftSection() {
+  const { t } = useTranslation("common");
+
   return (
     <section className="bg-upcrate-as-gift bg-center bg-cover text-center relative min-h-sectionBig md:min-h-sectionBigMd">
       <h3 className="font-display text-purple-dark text-2xl md:text-5xl absolute top-5 md:top-10 left-0 right-0">
-        upcrate as a gift
+        {t("sections.upcrate_as_gift.title")}
       </h3>
       <Button
         className="bg-orange text-white mt-20 absolute bottom-16 md:bottom-36 left-0 right-0"
         href="https://shop.upcrate.art/product/upcrategift/"
       >
-        Give a gift
+        {t("sections.upcrate_as_gift.cta")}
       </Button>
     </section>
   );
