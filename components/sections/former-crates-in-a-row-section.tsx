b29d9c4f07f238309ad1756b10723b0a794e0800
David <info@davidbucka.com>
2021-08-22 19:04:26 +0200

    use real data for former crates slider

--- components/sections/former-crates-in-a-row-section.tsx
+++ components/sections/former-crates-in-a-row-section.tsx
@@ -6,8 +6,12 @@ import React from "react";
 import { useWindowSize } from "../../hooks/use-window-size";
 import useTranslation from "next-translate/useTranslation";
 import { motion } from "framer-motion";
+import useIntersectionObserver from "../../hooks/use-intersection-observer";
+import { fetchCrates } from "../../utils/wc-api";
+import { Product } from "../../pages/crates";
+import Link from "next/link";
 
-function SliderArrow({
+export function SliderArrow({
   variant,
   onClick,
 }: {
@@ -46,13 +50,12 @@ function Slide({
   );
 }
 
-export function TripleSlider() {
+export function TripleSlider({ slides }: { slides: Product[] }) {
   const { width } = useWindowSize();
 
   const settings = {
     dots: false,
     arrows: true,
-    infinite: true,
     speed: 1000,
     autoplay: false,
     autoplaySpeed: 2000,
@@ -62,17 +65,23 @@ export function TripleSlider() {
     prevArrow: <SliderArrow variant="prev" />,
   };
 
-  const slides = Array.from(Array(20).keys());
+  if (!slides) {
+    return null;
+  }
 
   return (
     <Slider {...settings} className="md:mt-20 -mx-10 md:mx-0">
-      {slides.map((slide) => (
+      {slides.map((crate) => (
         <Slide
-          key={slide + 1}
-          className="flex flex-col gap-5 text-xl md:text-3xl"
+          key={crate.id}
+          className="flex flex-col gap-5 text-xl md:text-3xl cursor-pointer"
         >
-          <img src={`/home/former-crates/upcrate${slide + 1}.png`} />
-          <p>#upcrate{slide + 1}</p>
+          <Link href={crate.permalink}>
+            <div>
+              <img src={crate.images[0].src} />
+              <p className="pt-2">{crate.name}</p>
+            </div>
+          </Link>
         </Slide>
       ))}
     </Slider>
@@ -81,13 +90,42 @@ export function TripleSlider() {
 
 export function FormerCratesInARowSection() {
   const { t } = useTranslation("common");
+  const ref = React.useRef<HTMLDivElement | null>(null);
+  const entry = useIntersectionObserver(ref, {});
+  const isVisible = !!entry?.isIntersecting;
+  const [crates, setCrates] = React.useState<Product[]>();
+  const [isFetching, setIsFetching] = React.useState(false);
+
+  const fetchData = async () => {
+    setIsFetching(true);
+    const wcData = await fetchCrates().catch(console.error);
+
+    if (!wcData) {
+      setIsFetching(false);
+    }
+
+    setCrates(wcData.data);
+    setIsFetching(false);
+  };
+
+  React.useEffect(() => {
+    if (!isVisible || (crates && crates.length > 0)) {
+      return;
+    }
+
+    fetchData();
+  }, [isVisible]);
 
   return (
-    <section className="text-center text-rose bg-purple py-6 sm:px-10 md:px-32 md:pt-20 md:pb-24">
+    <section
+      className="text-center text-rose bg-purple py-6 sm:px-10 md:px-32 md:pt-20 md:pb-24 min-h-sectionBig md:min-h-sectionBigMd"
+      ref={ref}
+    >
       <h3 className="font-display text-2xl md:text-5xl">
         {t("sections.former_crates_in_a_row.title")}
       </h3>
-      <TripleSlider />
+
+      {isFetching ? "Loading data" : <TripleSlider slides={crates} />}
     </section>
   );
 }
