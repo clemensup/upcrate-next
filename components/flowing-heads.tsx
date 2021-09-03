c79ed51562bb612a5508efce9bac00996eccf148
David <info@davidbucka.com>
2021-09-03 09:16:23 +0200

    fix flowing heads

--- components/flowing-heads.tsx
+++ components/flowing-heads.tsx
@@ -1,7 +1,16 @@
+import Ticker from "react-ticker";
+import { CaptainCrateSvg } from "./elements/svg";
+
 export function FlowingHeads({ className }: { className?: string }) {
   return (
-    <div className={`${className} py-5 overflow-hidden`}>
-      <div className="bg-flowing-heads" style={{ height: 41 }}></div>
+    <div className={`${className} pb-2 pt-3 overflow-hidden`}>
+      <Ticker>
+        {() => (
+          <span className="inline-block mx-1">
+            <CaptainCrateSvg variant="head-only" />
+          </span>
+        )}
+      </Ticker>
     </div>
   );
 }
