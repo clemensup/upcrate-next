a5089227365381e496504650f8edc34ba557fb7d
David <info@davidbucka.com>
2021-09-24 09:20:50 +0200

    golive: add rewrites

--- next.config.js
+++ next.config.js
@@ -8,4 +8,23 @@ module.exports = nextTranslate({
   images: {
     domains: ["shop.upcrate.art"],
   },
+  async redirects() {
+    return [
+      {
+        source: "/refer-a-friend",
+        destination: "https://shop.upcrate.art/refer-a-friend",
+        permanent: true,
+      },
+      {
+        source: "/shop",
+        destination: "https://shop.upcrate.art/shop",
+        permanent: true,
+      },
+      {
+        source: "/account",
+        destination: "https://shop.upcrate.art/account",
+        permanent: true,
+      },
+    ];
+  },
 });
