64e495e78d59b8788ca71f05bf5e45f32b6cd784
David <info@davidbucka.com>
2021-10-04 13:27:01 +0200

    fix

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -2,12 +2,14 @@
 // The config you add here will be used whenever the server handles a request.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-import * as Sentry from '@sentry/nextjs';
+import * as Sentry from "@sentry/nextjs";
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'https://be9dcf23dbf744d5b209fb6098135106@o66054.ingest.sentry.io/5991303',
+  dsn:
+    SENTRY_DSN ||
+    "https://be9dcf23dbf744d5b209fb6098135106@o66054.ingest.sentry.io/5991303",
   // Adjust this value in production, or use tracesSampler for greater control
   tracesSampleRate: 1.0,
   // ...
