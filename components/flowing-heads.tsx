d38752a8151446c8cea4d52364ca56d4465d711a
David <info@davidbucka.com>
2021-11-21 22:53:45 +0100

    fix: moving heads on big screens

--- components/flowing-heads.tsx
+++ components/flowing-heads.tsx
@@ -1,16 +1,26 @@
+import React from "react";
 import Ticker from "react-ticker";
+import { useWindowSize } from "../hooks/use-window-size";
 import { CaptainCrateSvg } from "./elements/svg";
 
 export function FlowingHeads({ className }: { className?: string }) {
-  return (
-    <div className={`${className} pb-2 pt-3 overflow-hidden`}>
-      <Ticker>
-        {() => (
-          <span className="inline-block mx-1">
-            <CaptainCrateSvg variant="head-only" />
-          </span>
-        )}
-      </Ticker>
-    </div>
-  );
+  const { width } = useWindowSize();
+
+  if (width > 1440) {
+    return <></>;
+  }
+
+  return React.useMemo(() => {
+    return (
+      <div className={`${className} pb-2 pt-3 overflow-hidden`}>
+        <Ticker>
+          {() => (
+            <span className="inline-block mx-1">
+              <CaptainCrateSvg variant="head-only" />
+            </span>
+          )}
+        </Ticker>
+      </div>
+    );
+  }, []);
 }
