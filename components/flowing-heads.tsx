f689c887e18e88e1d0d63f99e7193d8906572bc6
David <info@davidbucka.com>
2021-11-23 21:59:27 +0100

    fix: try again big screens

--- components/flowing-heads.tsx
+++ components/flowing-heads.tsx
@@ -1,7 +1,14 @@
 import Ticker from "react-ticker";
+import { useWindowSize } from "../hooks/use-window-size";
 import { CaptainCrateSvg } from "./elements/svg";
 
 export function FlowingHeads({ className }: { className?: string }) {
+  const { width } = useWindowSize();
+
+  if (width > 1440) {
+    return <></>;
+  }
+
   return (
     <div className={`${className} pb-2 pt-3 overflow-hidden`}>
       <Ticker>
